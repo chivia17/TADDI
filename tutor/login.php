b3f344140145194bc17275cb68a77f0f72d42a92
chivia17 <ziranda.chivas@gmail.com>
2015-11-10 19:07:21 -0600

    Login tutor

---
+++ tutor/login.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+if (isset($_SESSION['usuario']))
+{
+	header('Location : index.php');
+}
+else{
+?>
+<!DOCTYPE html>
+<html>
+	<?php include "./includes/head.html"; ?>
+	<body>
+		<header class="cabecera">
+			<div class="row">
+				<div class="col-xs-3 col-sm-5 col-lg-8">
+					<img src="../img/logoTec.png" class="img-responsive">
+				</div>
+				<div class="col-xs-9 col-sm-7 col-lg-4">
+					<img src="../img/tadii.png" class="img-responsive">
+				</div>
+			</div>
+		</header>
+		<div id="background_login">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="text-left">
+						<button class="btn-back" onclick="location='../indexB.html'">
+							<span class="glyphicon glyphicon-arrow-left"></span>
+						</button>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div id="login_wrapper">
+					<img class="img-responsive" id="icon_user" src="../img/tutor_120.png" alt="">
+					<h3>TUTOR</h3>
+					<form method="POST" action="return false" onsubmit="return false">
+						<input class="form-control" id="userc" type="text" placeholder="Usuario">
+						<input class="form-control" id="passc" type="password"  placeholder="Contraseña">
+						<br>
+						<input class="btn btn-block" id="sesion-coor" type="submit" value="Iniciar sesión">
+					</form>
+				</div>
+			</div>
+		</div>
+		<footer>
+			<div id="foot">
+				<p class="text-right text-muted">
+					<br><br><a href="#">Ayuda y contacto &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+    			</p>
+     		</div>
+		</footer>
+	</body>
+</html>
+<?php
+}
+?>
