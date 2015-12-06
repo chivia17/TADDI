b77cc302ba22bcee0a4950a75e235e45cac2c333
chivia17 <ziranda.chivas@gmail.com>
2015-12-06 10:14:37 -0600

    Test Habilidades de Estudio
    Tutorado
    Actualizar la B.D a v6.1

--- js/tutorado.js
+++ js/tutorado.js
@@ -116,4 +116,37 @@ function main(){
     });
 
 
+    $('#grdHabilidades').click(function(){     //Boton Guardar Test Estilos Aprendizaje
+      p1 = $('input:radio[name=hab01]:checked').val();
+      p2 = $('input:radio[name=hab02]:checked').val();
+      p3 = $('input:radio[name=hab03]:checked').val();
+      p4 = $('input:radio[name=hab04]:checked').val();
+      p5 = $('input:radio[name=hab05]:checked').val();
+      p6 = $('input:radio[name=hab06]:checked').val();
+      p7 = $('input:radio[name=hab07]:checked').val();
+      p8 = $('input:radio[name=hab08]:checked').val();
+      p9 = $('input:radio[name=hab09]:checked').val();
+      p10 = $('input:radio[name=hab10]:checked').val();
+      p11 = $('input:radio[name=hab11]:checked').val();
+      p12 = $('input:radio[name=hab12]:checked').val();
+      p13 = $('input:radio[name=hab13]:checked').val();
+      p14 = $('input:radio[name=hab14]:checked').val();
+      p15 = $('input:radio[name=hab15]:checked').val();
+      p16 = $('input:radio[name=hab16]:checked').val();
+      p17 = $('input:radio[name=hab17]:checked').val();
+      p18 = $('input:radio[name=hab18]:checked').val();
+      p19 = $('input:radio[name=hab19]:checked').val();
+      p20 = $('input:radio[name=hab20]:checked').val();
+      $.post("./contents/testHabEst.php", {p1 : p1, p2 : p2, p3 : p3, p4 : p4,
+                                         p5 : p5, p6 : p6, p7 : p7, p8 : p8,
+                                     p9 : p9, p10 : p10, p11 : p11, p12 : p12,
+                                     p13 : p13, p14 : p14, p15 : p15, p16 : p16,
+                                     p17 : p17, p18 : p18, p19 : p19, p20 : p20
+                                           },
+                 function(data){
+                                alert(data);
+                              });
+    });
+
+
 }
