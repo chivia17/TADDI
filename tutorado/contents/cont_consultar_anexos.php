a5878876884e765f4107c378414fe1c54a5839b7
ErickOrtegaRodriguez <erick.ortega.rodriguez@outlook.es>
2015-12-09 12:04:50 -0600

    Encuesta PDF Tutorado
    PDF encuesta

--- tutorado/contents/cont_consultar_anexos.php
+++ tutorado/contents/cont_consultar_anexos.php
@@ -17,15 +17,35 @@
 		</tr>
 		<tr>
 			<td>Formato de entrevista</td>
-			<td class="text-center"><a href=""><span class="glyphicon glyphicon-eye-open"></span></a></td>
+			<td class="text-center"><a href="contents/cont_pdf_formato_entrevista.php?estado=1"><span class="glyphicon glyphicon-eye-open"></span></a></td>
 		</tr>
 		<tr>
 			<td>Línea de vida</td>
-			<td class="text-center"><a href=""><span class="glyphicon glyphicon-eye-open"></span></a></td>
+			<td class="text-center"><a href="contents/cont_pdf_formato_entrevista.php?estado=2"><span class="glyphicon glyphicon-eye-open"></span></a></td>
 		</tr>
 		<tr>
 			<td>Análisis FODA</td>
-			<td class="text-center"><a href=""><span class="glyphicon glyphicon-eye-open"></span></a></td>
+			<td class="text-center"><a href="contents/cont_pdf_formato_entrevista.php?estado=3"><span class="glyphicon glyphicon-eye-open"></span></a></td>
+		</tr>
+		<tr>
+			<td>Encuesta sobre las Habilidades de Estudio</td>
+			<td class="text-center"><a href="contents/cont_pdf_formato_entrevista.php?estado=4"><span class="glyphicon glyphicon-eye-open"></span></a></td>
+		</tr>
+		<tr>
+			<td>Inventario sobre Estilos de Aprendizaje</td>
+			<td class="text-center"><a href="contents/cont_pdf_formato_entrevista.php?estado=5"><span class="glyphicon glyphicon-eye-open"></span></a></td>
+		</tr>
+		<tr>
+			<td>Test de Autoestima</td>
+			<td class="text-center"><a href="contents/cont_pdf_formato_entrevista.php?estado=6"><span class="glyphicon glyphicon-eye-open"></span></a></td>
+		</tr>
+		<tr>
+			<td>Test de Asertividad</td>
+			<td class="text-center"><a href="contents/cont_pdf_formato_entrevista.php?estado=7"><span class="glyphicon glyphicon-eye-open"></span></a></td>
+		</tr>
+		<tr>
+			<td>Rúbrica para evaluar el desempeño del tutor</td>
+			<td class="text-center"><a href="contents/cont_pdf_formato_entrevista.php?estado=8"><span class="glyphicon glyphicon-eye-open"></span></a></td>
 		</tr>
 	</table>
 </div>
