be54152fee76f0b0d259dcadb2ad9afa0ed0e0b2
ErickOrtegaRodriguez <erick.ortega.rodriguez@outlook.es>
2015-12-09 02:40:16 -0600

    Consultar Encuesta Tutor
    Falta Consultar encuesta en Tutorado

--- tutor/contents/cont_consultar_encuestas.php
+++ tutor/contents/cont_consultar_encuestas.php
@@ -1,25 +1,51 @@
+<?php
+
+@session_start();
+include 'conexion.php';
+$alumno = "";
+$carrera = "";
+$semestre = "";
+$grupo = "";
+$no_control="";
+if(isset($_POST['noControl'])){
+	$conexion = conectar();
+	$query = "SELECT idAlumnos,nombreAlumno, apellPAlumno, apellMAlumno, cveCarrera,  NombreGrupo FROM alumnos INNER JOIN grupos ON alumnos.idGrupos = grupos.idGrupos WHERE idAlumnos = '".$_POST['noControl']."'";
+	if($result = $conexion->query($query)){
+		if($result->num_rows == 1){
+			$array = mysqli_fetch_array($result);
+			$alumno = $array['nombreAlumno']." ".$array['apellPAlumno']." ".$array['apellMAlumno'];
+			$carrera = $array['cveCarrera'];
+			$grupo = $array['NombreGrupo'];
+			$no_control = $array['idAlumnos'];
+		}
+		else
+			echo '<script language="javascript">alert("Alumno no registrado!");</script>';
+	}
+}
+?>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <div class="row">
 	<div class="titulo text-center">
 		<h2><b>ENCUESTAS</b></h2>
 	</div>
 </div>
-
 <br><br>		
-<form role="form" method="post" action="">
+<form role="form" method="post" action = "consultar_encuestas.php">
 	<div class="row">
 		<div class="form-horizontal">
 			<div class="form-group">
 				<label for="noControl" class="col-sm-4 control-label">Numero de Control</label>
 				<div class="col-sm-4">
-				    <input type="text" class="form-control" id="noControl" name="noControl" placeholder="Número de control" required>
+				    <input type="text" class="form-control" id="noControl" name="noControl" placeholder="Número de control" value = <?php echo $no_control; ?> required>
 				</div>
 				<div class="col-sm-2">
-      				<button type="submit" class="btn btn-default btn-block">Buscar</button>
+      				<button type="submit" class="btn btn-default btn-block" onclick = "buscar()">Buscar</button>
     			</div>
 			</div>
 		</div>
 	</div>
+<form/>
+<form role = "form" method = "post" action = ""> 
 	<hr>	
 	<br>
 	<div class="row">
@@ -27,21 +53,21 @@
   			<div class="form-group">
     			<label for="nomAlumno" class="col-sm-1 control-label">Alumno</label>
     			<div class="col-sm-11">
-	      			<input type="text" class="form-control" id="nomAlumno" name="" readonly>
+	      			<input type="text" class="form-control" id="nomAlumno" name="" readonly value = "<?php echo $alumno;  ?>">
     			</div>
   			</div>
   			<div class="form-group">
     			<label for="carAlumno" class="col-sm-1 control-label">Carrera</label>
     			<div class="col-sm-6">
-      				<input type="text" class="form-control" id="carAlumno" name="" readonly>
+      				<input type="text" class="form-control" id="carAlumno" name="" readonly value = "<?php echo $carrera; ?>">
     			</div>
     			<label for="semAlumno" class="col-sm-1 control-label">Semestre</label>
     			<div class="col-sm-1">
-      				<input type="text" class="form-control" id="semAlumno" name="" readonly>
+      				<input type="text" class="form-control" id="semAlumno" name="" readonly value = "<?php echo $semestre; ?>">
     			</div>
     			<label for="gpoAlumno" class="col-sm-1 control-label">Grupo</label>
     			<div class="col-sm-2">
-      				<input type="text" class="form-control" id="gpoAlumno" name="" readonly>
+      				<input type="text" class="form-control" id="gpoAlumno" name="" readonly value = "<?php echo $grupo; ?>">
     			</div>
   			</div>
 		</div>
@@ -59,15 +85,35 @@
 						</tr>
 						<tr>
 					  		<td>Formato de entrevista</td>
-						  	<td class="text-center"><a href=""><span class="glyphicon glyphicon-eye-open"></span></a></td>
+						  	<td class="text-center"><a href="contents/cont_pdf_formato_entrevista.php?no_control=<?php echo $no_control;?>&estado=1"><span class="glyphicon glyphicon-eye-open"></span></a></td>
 						</tr>
 						<tr>
 							<td>Línea de vida</td>
-							<td class="text-center"><a href=""><span class="glyphicon glyphicon-eye-open"></span></a></td>
+							<td class="text-center"><a href="contents/cont_pdf_formato_entrevista.php?no_control=<?php echo $no_control;?>&estado=2"><span class="glyphicon glyphicon-eye-open"></span></a></td>
 						</tr>
 						<tr>
 							<td>Análisis FODA</td>
-							<td class="text-center"><a href=""><span class="glyphicon glyphicon-eye-open"></span></a></td>
+							<td class="text-center"><a href="contents/cont_pdf_formato_entrevista.php?no_control=<?php echo $no_control;?>&estado=3"><span class="glyphicon glyphicon-eye-open"></span></a></td>
+						</tr>
+						<tr>
+							<td>Encuesta sobre las Habilidades de Estudio</td>
+							<td class="text-center"><a href="contents/cont_pdf_formato_entrevista.php?no_control=<?php echo $no_control;?>&estado=4"><span class="glyphicon glyphicon-eye-open"></span></a></td>
+						</tr>
+						<tr>
+							<td>Inventario sobre Estilos de Aprendizaje</td>
+							<td class="text-center"><a href="contents/cont_pdf_formato_entrevista.php?no_control=<?php echo $no_control;?>&estado=5"><span class="glyphicon glyphicon-eye-open"></span></a></td>
+						</tr>
+						<tr>
+							<td>Test de Autoestima</td>
+							<td class="text-center"><a href="contents/cont_pdf_formato_entrevista.php?no_control=<?php echo $no_control;?>&estado=6"><span class="glyphicon glyphicon-eye-open"></span></a></td>
+						</tr>
+						<tr>
+							<td>Test de Asertividad</td>
+							<td class="text-center"><a href="contents/cont_pdf_formato_entrevista.php?no_control=<?php echo $no_control;?>&estado=7"><span class="glyphicon glyphicon-eye-open"></span></a></td>
+						</tr>
+						<tr>
+							<td>Rúbrica para evaluar el desempeño del tutor</td>
+							<td class="text-center"><a href="contents/cont_pdf_formato_entrevista.php?no_control=<?php echo $no_control;?>&estado=8"><span class="glyphicon glyphicon-eye-open"></span></a></td>
 						</tr>
 	  				</table>
 			</div>
@@ -84,4 +130,6 @@
 	
 	
 </form>
+
+
 
---
+++ tutor/contents/cont_pdf_formato_entrevista.php
@@ -0,0 +1,25 @@
+<?php
+include'conexion.php';
+include_once('PDF_Encuesta.php');
+$no_control= $_GET['no_control'];
+$estado = $_GET['estado'];
+$conexion = conectar();
+$array = array();
+$query = "SELECT DescripcionPregunta, DescricionRespuesta FROM (respuestas INNER JOIN preguntas ON  respuestas.idPregunta = preguntas.idPreguntas) INNER JOIN anexos ON preguntas.idAnexo = anexos.idAnexo WHERE idAlumnos = '$no_control' AND anexos.idAnexo = ".$estado." ORDER BY idPregunta ASC" or die("Error in the consult.." . mysqli_error($conexion));
+if($result = $conexion->query($query)){
+	if($result->num_rows > 0){
+		$pdf = new PDF();
+		$pdf->AddPage();
+		while($row = mysqli_fetch_assoc($result)){
+			array_push($array, $row);
+		}
+		$Cabecera = array('Pregunta', 'Respuesta');
+		//$array = mysqli_fetch_array($result);
+		$pdf->tablaHorizontal($Cabecera, $array);
+		$pdf->Output();
+	}
+	else{
+		echo '<script language="javascript">alert("No se ha contestado esta encuesta!"); window.location.href="../consultar_encuestas.php";</script>';
+	}
+}
+?>
