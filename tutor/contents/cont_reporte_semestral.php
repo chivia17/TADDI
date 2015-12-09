3693018c90a12cb3eacd99af552ea3149aab4465
JorgeeEnriquee <jorgee.enriqee@gmail.com>
2015-12-09 00:07:03 -0600

    El commit malo
    Tengo que hacer este commit para poder bajar la versión de alan.
    BBB

--- tutor/contents/cont_reporte_semestral.php
+++ tutor/contents/cont_reporte_semestral.php
@@ -1,3 +1,7 @@
+<?php
+$cve = $_SESSION["rfc"];
+$carrera=$_SESSION["cveCar"];
+ ?>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <div class="row">
 	<div class="text-center">
@@ -11,15 +15,7 @@
 	<div class="row">
 		<div class="form-horizontal">
   			<div class="form-group">
-    			<label for="selGrupo" class="col-sm-2 control-label">Grupo</label>
-    			<div class="col-sm-7">
-	      			<select required name="selGrupo" class="form-control" id="selGrupo">
-                		<option value="">--Selecciona grupo--</option>
-  					</select><br>
-    			</div>
-    			<div class="col-sm-2">
-      				<button type="submit" class="btn btn-primary btn-block">Ver listado</button>
-    			</div>
+    			
   			</div>
 		</div>
 		<hr>
@@ -28,7 +24,7 @@
 	<br>
 	<div class="row">
 		<div class="panel panel-success">
-			<div class="panel-heading">ISC-20151</div>
+			<div class="panel-heading">Reporte</div>
 			<div class="table-responsive">
 				<table class="table table-bordered table-hover">
 					<tr>
@@ -37,52 +33,127 @@
 					<tr>
 						<td colspan="7">Instituto Tecnológico de Morelia</td>
 					</tr>
-					<tr>
-						<td colspan="2">Nombre del tutor:</td>
-						<td colspan="3">María Yaneth Vega Flores</td>
-						<td colspan="1">Fecha:</td>
-						<td colspan="1">
-							<input type="date" class="form-control" name="fecReporte">
-						</td>
-					</tr>
-					<tr>
-						<td colspan="2">Programa educativo:</td>
-						<td colspan="1"></td>
-						<td colspan="1">Grupo:</td>
-						<td colspan="1">ISC-20151</td>
-						<td colspan="1">Hora:</td>
-						<td colspan="1" contenteditable="true"></td>
-					</tr>
-					<tr>
-						<th>No.</th>
-						<th>No. de control</th>
-						<th>Lista de estudiantes</th>
-						<th>Calificación</th>
-						<th>% Asistencia</th>
-						<th>Área canalizada</th>
-						<th>Observaciones</th>
-					</tr>
-					<tr>
-						<td>1</td>
-						<td>14121253</td>
-						<td>Cesar Rafael Aguado Boyzo</td>
-						<td contenteditable="true"></td>
-						<td contenteditable="true"></td>
-						<td contenteditable="true"></td>
-						<td contenteditable="true"></td>
-					</tr>
-					<tr>
-						<td>2</td>
-						<td>14121254</td>
-						<td>Sergio Armando Alcazar Calderón</td>
-						<td contenteditable="true"></td>
-						<td contenteditable="true"></td>
-						<td contenteditable="true"></td>
-						<td contenteditable="true"></td>
-					</tr>
+					<?php                					
+							        	$servidor = "localhost";
+							        	$basedatos = "mydb";
+							        	$usuario = "root";
+							        	$contrasena = "";
+
+								        $conexion = new PDO("mysql:host=$servidor;dbname=$basedatos",
+							                            $usuario,
+							                            $contrasena,
+							                            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+
+							    	    $conexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+							        	
+							    	$resp=false;
+							        $consulta="Select concat(nombreTutor,' ',apellPTutor,' ',apellMTutor) as nombre,idTutores from Tutores where idTutores='$cve'";
+							        $sentencia=$conexion->prepare($consulta);
+							        try {
+							        if(!$sentencia->execute()){
+							            print_r($sentencia->errorInfo());
+							            echo "<script>alert('Error en la consulta')</script>";
+							        }
+							        $resp = $sentencia->fetchAll();
+							        if(!$resp){
+        								echo "<script>alert('$cve')</script>";
+							        	echo "<script>alert('Resp vacio')</script>";
+							        }							       
+							        $sentencia->closeCursor();
+							        }
+							        catch(PDOException $e){
+							            echo "Error al ejecutar la sentencia: \n";
+							            print_r($e->getMessage());
+							        }
+							        echo "<tr>";				
+							        echo "<td colspan=\"2\">Nombre del tutor:</td>	";			
+									foreach ($resp as $indice => $registro) {        								
+        								echo "<td colspan=\"3\">".$registro["nombre"]."</td>";
+        							}        							
+        							echo "<td colspan=\"1\">Fecha:</td>";
+									echo "<td colspan=\"1\">";
+									echo "<input type=\"date\" class=\"form-control\" name=\"fecReporte\">";
+									echo "</td>";
+									echo "</tr>";
+							?>
+							<?php
+									$servidor = "localhost";
+							        	$basedatos = "mydb";
+							        	$usuario = "root";
+							        	$contrasena = "";
+
+								        $conexion = new PDO("mysql:host=$servidor;dbname=$basedatos",
+							                            $usuario,
+							                            $contrasena,
+							                            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+
+							    	    $conexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+							        	
+							    	$resp=false;
+							        $consulta="select DescricionRespuesta from (select idAlumnos from alumnos inner join grupos on alumnos.idGrupos=grupos.idGrupos where grupos.cveCarrera='$carrera') x inner join
+respuestas on x.idAlumnos=respuestas.idAlumnos where idAnexo=9 and idSubanexos=9;";
+							        $sentencia=$conexion->prepare($consulta);
+							        try {
+							        if(!$sentencia->execute()){
+							            print_r($sentencia->errorInfo());
+							            echo "<script>alert('Error en la consulta')</script>";
+							        }
+							        $resp = $sentencia->fetchAll();
+							        if(!$resp){
+        								echo "<script>alert('$cve')</script>";
+							        	echo "<script>alert('Resp vacio')</script>";
+							        }							       
+							        $sentencia->closeCursor();
+							        }
+							        catch(PDOException $e){
+							            echo "Error al ejecutar la sentencia: \n";
+							            print_r($e->getMessage());
+							        }
+									echo "<tr>";
+									echo "<td colspan=\"2\">Programa educativo:</td>";
+									echo "<td colspan=\"1\">".$carrera." </td>";
+									echo "<td colspan=\"1\">Grupo:</td>";
+									echo "<td colspan=\"1\">Todos los grupos</td>";
+									echo "<td colspan=\"1\">Hora:</td>";
+									echo "<td colspan=\"1\" contenteditable=\"false\"></td>";
+									echo "</tr>";
+
+									echo "<tr>";
+									echo "<th>No.</th>";
+									echo "<th>No. de control</th>";
+									echo "<th>Lista de estudiantes</th>";
+									echo "<th>Calificación</th>";
+									echo "<th>% Asistencia</th>";
+									echo "<th>Área canalizada</th>";
+									echo "<th>Observaciones</th>";
+									echo "</tr>";
+									$cont=1;
+									foreach ($resp as $indice => $registro) {        								
+        								//echo "<td colspan=\"3\">".$registro["nombre"]."</td>";
+        								if(($cont/5)==0||$cont==1){
+        									echo "<tr>";	
+        									echo "<td>".$cont."</td>";
+        								}										
+										echo "<td>".$registro["DescricionRespuesta"]."</td>";
+										if($cont/6==0){
+        									echo "<7tr>";	
+        								}  
+										$cont=$cont+1;
+        							}				
+        							if(isset($_POST['nuevo'])){
+        								echo "<tr>";
+        								echo "<td></td>";
+        								echo "<td></td>";
+        								echo "<td></td>";
+        								echo "<td></td>";
+        								echo "<td></td>";
+        								echo "<td></td>";
+        								echo "</tr>";
+        							}					
+						?>									
 					<tr>
 						<td colspan="2">Observaciones:</td>
-						<td colspan="5" contenteditable="true"></td>
+						<td colspan="5" contenteditable="false"></td>
 					</tr>
 				</table>
 			</div>
@@ -101,10 +172,9 @@
 			<button type="submit" class="btn btn-info btn-block">Generar PDF</button>
 		</div>
 		<div class="col-xs-6 col-sm-3">
-			<button type="submit" class="btn btn-warning btn-block">Imprimir</button>
+			<button type="submit" class="btn btn-warning btn-block" name="nuevo">Imprimir</button>
 		</div>
 	</div>
 	<br>
 	
 </form>
-
