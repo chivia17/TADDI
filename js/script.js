68774aed17a832ee9ad3b4d21ada4d87ec9c3b5a
chivia17 <ziranda.chivas@gmail.com>
2016-03-08 12:32:13 -0600

    Cambio nomenclatura alta carrera
    gracias a los culeros de los requerimientos ¬_¬

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ function validar_changePsw(){
 	document.getElementById("errors_changePsw").innerHTML = "";
 	pswNew = document.getElementById("pswNew").value;
 	pswChk = document.getElementById("pswChk").value;
-	
-	
-	if(pswNew.length < 6){ 
+
+
+	if(pswNew.length < 6){
 			document.getElementById("errors_changePsw").innerHTML = "<p class='text-center text-danger'>¡La contraseña debe contener al menos 6 caracteres!</p>";
 		return false;
 	}
@@ -17,12 +17,12 @@ function validar_changePsw(){
 
 function validar_alta_carr(le,Le2){
 	document.getElementById("errors_carr").innerHTML = "";
-	val2 = document.getElementById(le).value;	
+	val2 = document.getElementById(le).value;
 	val3=document.getElementById(Le2).value;
-		if (validar_cve(val2)){			
+		if (validar_cve(val2)){
 			if(validar_let(val3)){
 				return true;
-			}			
+			}
 
 			else {
 				document.getElementById("errors_carr").innerHTML = "<p class='text-center text-danger'>¡Formato de nombre Incorrecto!</p>";
@@ -142,7 +142,7 @@ function validar_rfc(rf) {
 		else
 		{
 			return true;
-		}		
+		}
 	}
 
 	else if(rfc.length==9){
@@ -200,7 +200,7 @@ function validar_let(let){
 
 function validar_cve(let){
 	valor = let;
-	var patron = /^[A-Z]{4}-[0-9]{4}-[0-9]{3}$/;
+	var patron = /^[A-Z]{4}$/;
     if(!valor.search(patron)) {
 	  return true;
 	}
@@ -224,7 +224,7 @@ function validar_correo(cor) {
 }
 
 function validar_nc(nc){
-	var control=document.getElementById(nc).value;	
+	var control=document.getElementById(nc).value;
 	var token=/^[0-9]{8}$/;
 	if(!control.search(token)){
 		alert('El numero de control está bien');
